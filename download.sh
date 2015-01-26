9cbd4d26729c122d3fdc6a3ceb440b2db0035669
Frank R. <renfeng.cn@gmail.com>
2015-01-27 04:43:46 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -4,3 +4,7 @@ wget -N https://dl-ssl.google.com/android/repository/repository-10.xml
 
 # http://stackoverflow.com/questions/8535947/xslt-2-0-transformation-via-linux-shell
 java -jar /usr/share/java/saxon.jar repository-10.xml repository-10.xsl | wget -N -i -
+
+# make urls relative and local
+cp repository-10.xml repository-10.orig
+sed -i 's/https:\/\/dl-ssl.google.com\/android\/repository\///g' repository-10.xml
