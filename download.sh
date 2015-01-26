1eff9c951238949a06050ebfd8ee0cd89cb19127
Frank R. <renfeng.cn@gmail.com>
2015-01-27 03:04:58 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,2 +1,4 @@
 wget http://dl-ssl.google.com/android/repository/repository-10.xml
+
+# http://stackoverflow.com/questions/8535947/xslt-2-0-transformation-via-linux-shell
 java -jar /usr/share/java/saxon.jar repository-10.xml repository-10.xsl | wget -i -
