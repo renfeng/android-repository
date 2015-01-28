4ee7220dfca4d5a8ad7772e2932e41556cc5a171
Frank R <renfeng.cn@gmail.com>
2015-01-29 04:07:46 +0800

    added client scripts, and a meta file

---
+++ client/clean.sh
@@ -0,0 +1,3 @@
+# these files will be restored to their defaults next time you run Android SDK Manager, Tools > Manage Add-on Sites... > Close
+rm ~/.android/repositories.cfg
+rm ~/.android/sites-settings.cfg
