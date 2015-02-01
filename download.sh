40550ce89956adfb311385c1f9d03d7c16cd7c46
Frank R <renfeng.cn@gmail.com>
2015-02-01 08:42:15 +0800

    modified and tested with latest cygwin on win7

--- download.sh
+++ download.sh
@@ -43,19 +43,19 @@ wget -N https://dl-ssl.google.com/android/repository/sys-img/android/sys-img.xml
 wget -N https://dl-ssl.google.com/glass/gdk/addon.xml -P glass/gdk
 
 # http://stackoverflow.com/questions/8535947/xslt-2-0-transformation-via-linux-shell
-java -jar /usr/share/java/saxon.jar android/repository/extras/intel/addon.xml $BASEDIR/android/repository/extras/intel/addon.xsl | wget -N -P android/repository/extras/intel -i -
-java -jar /usr/share/java/saxon.jar android/repository/addon-6.xml $BASEDIR/android/repository/addon-6.xsl | wget -N -P android/repository -i -
-java -jar /usr/share/java/saxon.jar android/repository/sys-img/android-tv/sys-img.xml $BASEDIR/android/repository/sys-img/android-tv/sys-img.xsl | wget -N -P android/repository/sys-img/android-tv -i -
+java -jar $BASEDIR/saxon.jar android/repository/extras/intel/addon.xml $BASEDIR/android/repository/extras/intel/addon.xsl | wget -N -P android/repository/extras/intel -i -
+java -jar $BASEDIR/saxon.jar android/repository/addon-6.xml $BASEDIR/android/repository/addon-6.xsl | wget -N -P android/repository -i -
+java -jar $BASEDIR/saxon.jar android/repository/sys-img/android-tv/sys-img.xml $BASEDIR/android/repository/sys-img/android-tv/sys-img.xsl | wget -N -P android/repository/sys-img/android-tv -i -
 # requires 4GB
-java -jar /usr/share/java/saxon.jar android/repository/repository-10.xml $BASEDIR/android/repository/repository-10.xsl | wget -N -P android/repository -i -
-java -jar /usr/share/java/saxon.jar android/repository/sys-img/android-wear/sys-img.xml $BASEDIR/android/repository/sys-img/android-wear/sys-img.xsl | wget -N -P android/repository/sys-img/android-wear -i -
-java -jar /usr/share/java/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.xsl | wget -N -P android/repository -i -
-java -jar /usr/share/java/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.admob.xsl | wget -N -P googleadmobadssdk -i -
-java -jar /usr/share/java/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.analytics.xsl | wget -N -P gaformobileapps -i -
-java -jar /usr/share/java/saxon.jar android/repository/sys-img/x86/addon-x86.xml $BASEDIR/android/repository/sys-img/x86/addon.xsl | wget -N -P android/repository/sys-img/x86 -i -
-java -jar /usr/share/java/saxon.jar android/repository/sys-img/google_apis/sys-img.xml $BASEDIR/android/repository/sys-img/google_apis/sys-img.xsl | wget -N -P android/repository/sys-img/google_apis -i -
-java -jar /usr/share/java/saxon.jar android/repository/sys-img/android/sys-img.xml $BASEDIR/android/repository/sys-img/android/sys-img.xsl | wget -N -P android/repository/sys-img/android -i -
-java -jar /usr/share/java/saxon.jar glass/gdk/addon.xml $BASEDIR/glass/gdk/addon.xe22.xsl | wget -N -P glass/xe22 -i -
+java -jar $BASEDIR/saxon.jar android/repository/repository-10.xml $BASEDIR/android/repository/repository-10.xsl | wget -N -P android/repository -i -
+java -jar $BASEDIR/saxon.jar android/repository/sys-img/android-wear/sys-img.xml $BASEDIR/android/repository/sys-img/android-wear/sys-img.xsl | wget -N -P android/repository/sys-img/android-wear -i -
+java -jar $BASEDIR/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.xsl | wget -N -P android/repository -i -
+java -jar $BASEDIR/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.admob.xsl | wget -N -P googleadmobadssdk -i -
+java -jar $BASEDIR/saxon.jar android/repository/addon.xml $BASEDIR/android/repository/addon.analytics.xsl | wget -N -P gaformobileapps -i -
+java -jar $BASEDIR/saxon.jar android/repository/sys-img/x86/addon-x86.xml $BASEDIR/android/repository/sys-img/x86/addon.xsl | wget -N -P android/repository/sys-img/x86 -i -
+java -jar $BASEDIR/saxon.jar android/repository/sys-img/google_apis/sys-img.xml $BASEDIR/android/repository/sys-img/google_apis/sys-img.xsl | wget -N -P android/repository/sys-img/google_apis -i -
+java -jar $BASEDIR/saxon.jar android/repository/sys-img/android/sys-img.xml $BASEDIR/android/repository/sys-img/android/sys-img.xsl | wget -N -P android/repository/sys-img/android -i -
+java -jar $BASEDIR/saxon.jar glass/gdk/addon.xml $BASEDIR/glass/gdk/addon.xe22.xsl | wget -N -P glass/xe22 -i -
 
 # make urls relative and local
 cp android/repository/addons_list-2.xml android/repository/addons_list-2.xml.orig
