558e216aaee801732df1a771098945a283d976bc
Frank R <renfeng.cn@gmail.com>
2015-07-31 13:32:29 +0800

    will not download exe files

--- download.sh
+++ download.sh
@@ -7,8 +7,7 @@
 # it doesn't download when internet is unavailable, and android studio works just fine
 
 rm -f orig/sdk/index.html
-wget http://developer.android.com/sdk/index.html \
-                             -P orig/sdk
+wget http://developer.android.com/sdk/index.html -P orig/sdk
 
 # sed remove lines until
 # http://www.linuxquestions.org/questions/linux-newbie-8/how-to-use-sed-to-delete-all-lines-before-the-first-match-of-a-pattern-802069/
@@ -16,24 +15,25 @@ wget http://developer.android.com/sdk/index.html \
 # http://stackoverflow.com/questions/5227295/how-do-i-delete-all-lines-in-a-file-starting-from-after-a-matching-line
 cat orig/sdk/index.html | sed -n '/pax/,$p' | sed '/end pax/q' | tee orig/sdk/index.html
 
-#grep -Po 'https://dl.google.com/dl/android/studio/install/1.0.1/[^"]*' orig/sdk/index.html | wget -N -P dl/android/studio/install/1.0.1 -c -i -
-#grep -Po 'https://dl.google.com/dl/android/studio/ide-zips/1.0.1/[^"]*' orig/sdk/index.html | wget -N -P dl/android/studio/ide-zips/1.0.1 -c -i -
-grep -o 'https://dl.google.com/dl/android/studio/[^"]*' orig/sdk/index.html > \
-                                dl/android/studio/download.sh
-cat dl/android/studio/download.sh | sed -E 's/https:(\/\/dl.google.com\/(dl\/android\/studio\/[^\/]+\/[^\/]+)\/.+)/wget -N -P \2 -c http:\1/g' | tee dl/android/studio/download.sh
+# download android studio
+grep -o 'https://dl.google.com/dl/android/studio/[^"]*' orig/sdk/index.html | \
+          grep -v [.]exe | tee dl/android/studio/download.sh
+cat dl/android/studio/download.sh | \
+    sed -E 's/https:(\/\/dl.google.com\/(dl\/android\/studio\/[^\/]+\/[^\/]+)\/.+)/wget -N -P \2 -c http:\1/g' | \
+    tee dl/android/studio/download.sh
 sh dl/android/studio/download.sh
-grep -o 'http://dl.google.com/android/[^"]*' orig/sdk/index.html | \
-                    wget -N -P android -c -i -
 
-sed '/<!-- insert -->/q' sdk/template.html > sdk/index.html.tmp
+# download sdk manager
+grep -o 'http://dl.google.com/android/[^"]*' orig/sdk/index.html | 
+    grep -v [.]exe | wget -N -P android -c -i -
+
+cat sdk/template.html | sed '/<!-- insert -->/q' | tee sdk/index.html
 cat orig/sdk/index.html | \
     sed 's/https:\/\/dl.google.com//g' | \
     sed 's/http:\/\/dl.google.com//g' | \
-    sed 's/onclick="return onDownload(this)"/target="_blank"/g' >> \
-         sdk/index.html.tmp
-sed -n '/<!-- insert -->/,$p' sdk/template.html >> \
-         sdk/index.html.tmp
-mv sdk/index.html.tmp sdk/index.html
+    sed 's/onclick="return onDownload(this)"/target="_blank"/g' | \
+    tee -a sdk/index.html
+cat sdk/template.html | sed -n '/<!-- insert -->/,$p' | tee -a sdk/index.html
 
 # requires 25GB
 
@@ -45,7 +45,7 @@ $BASEDIR/sync-index.sh
 $BASEDIR/manage.sh
 
 # clean obsolete sdk packages
-grep true packages.csv | grep -Po '(?<=https://dl-ssl[.]google[.]com/)[^,]+|(?<=https://dl[.]google[.]com/)[^,]+' | sed -r 's/^(.*)$/rm -f \1/g' > clean-obsolete.sh
+grep true packages.csv | grep -Po '(?<=https://dl-ssl[.]google[.]com/)[^,]+|(?<=https://dl[.]google[.]com/)[^,]+' | sed -r 's/^(.*)$/rm -f \1/g' | tee clean-obsolete.sh
 sh clean-obsolete.sh
 
 # http://stackoverflow.com/questions/8535947/xslt-2-0-transformation-via-linux-shell
@@ -101,41 +101,41 @@ java -jar $BASEDIR/saxon.jar orig/glass/gdk/addon.xml \
 # make urls relative and local
 
 # glass should be treated as a root path
-sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' \
-    orig/android/repository/addons_list-2.xml > \
-         android/repository/addons_list-2.xml
+cat orig/android/repository/addons_list-2.xml | \
+    sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' | \
+     tee android/repository/addons_list-2.xml
 
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/extras/intel/addon.xml > \
-         android/repository/extras/intel/addon.xml
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/addon-6.xml > \
-         android/repository/addon-6.xml
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/sys-img/android-tv/sys-img.xml > \
-         android/repository/sys-img/android-tv/sys-img.xml
-sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' \
-    orig/android/repository/repository-10.xml > \
-         android/repository/repository-10.xml
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/sys-img/android-wear/sys-img.xml > \
-         android/repository/sys-img/android-wear/sys-img.xml
+cat orig/android/repository/extras/intel/addon.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/extras/intel/addon.xml
+cat orig/android/repository/addon-6.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/addon-6.xml
+cat orig/android/repository/sys-img/android-tv/sys-img.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/sys-img/android-tv/sys-img.xml
+cat orig/android/repository/repository-10.xml | \
+    sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' | \
+     tee android/repository/repository-10.xml
+cat orig/android/repository/sys-img/android-wear/sys-img.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/sys-img/android-wear/sys-img.xml
 cat orig/android/repository/addon.xml | \
     sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' | \
-    sed 's/https:\/\/dl.google.com/http:\/\/studyjams.dushu.hu/g' > \
-         android/repository/addon.xml
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/sys-img/x86/addon-x86.xml > \
-         android/repository/sys-img/x86/addon-x86.xml
-sed 's/https:\/\/dl-ssl.google.com//g' \
-    orig/android/repository/sys-img/google_apis/sys-img.xml > \
-         android/repository/sys-img/google_apis/sys-img.xml
-sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' \
-    orig/android/repository/sys-img/android/sys-img.xml > \
-         android/repository/sys-img/android/sys-img.xml
-sed 's/https:\/\/dl.google.com/http:\/\/studyjams.dushu.hu/g' \
-    orig/glass/gdk/addon.xml > \
-         glass/gdk/addon.xml
+    sed 's/https:\/\/dl.google.com/http:\/\/studyjams.dushu.hu/g' | \
+     tee android/repository/addon.xml
+cat orig/android/repository/sys-img/x86/addon-x86.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/sys-img/x86/addon-x86.xml
+cat orig/android/repository/sys-img/google_apis/sys-img.xml | \
+    sed 's/https:\/\/dl-ssl.google.com//g' | \
+     tee android/repository/sys-img/google_apis/sys-img.xml
+cat orig/android/repository/sys-img/android/sys-img.xml | \
+    sed 's/https:\/\/dl-ssl.google.com/http:\/\/studyjams.dushu.hu/g' | \
+     tee android/repository/sys-img/android/sys-img.xml
+cat orig/glass/gdk/addon.xml | \
+    sed 's/https:\/\/dl.google.com/http:\/\/studyjams.dushu.hu/g' | \
+     tee glass/gdk/addon.xml
 
 # verify
 grep -rn '<sdk:url>' * --include=*.xml --exclude-dir=orig | grep http
